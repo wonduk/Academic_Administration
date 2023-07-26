d4a8b6b99c2544138b5f2e8e90bc11269fe9af2a
wonduk <122516138+wonduk@users.noreply.github.com>
2023-07-26 19:53:28 +0900

    Add files via upload

---
+++ Haksa2/src/Operator.java
@@ -0,0 +1,13 @@
+public class Operator {
+	Database db = null;
+	MainFrame mf = null;
+	JoinFrame jf = null;
+	
+	public static void main(String[] args) {
+		Operator opt = new Operator();
+		opt.db = new Database();
+		opt.mf = new MainFrame(opt);
+		opt.jf = new JoinFrame(opt);
+
+	}
+}
